736c1cb6fe831a71eb37ecdbcc9a42bd792b12a9
ck kim <keegi@kakao.com>
2022-08-24 17:45:22 +0900

    useParams 및 css 적용

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App()
     <Router>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/movie" element={<Detail />} />
+        <Route path="/movie/:id" element={<Detail />} />
       </Routes>
     </Router>
   );
